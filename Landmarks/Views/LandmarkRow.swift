19bdeb7071397a56ed142871afe8e23b77150076
Mofe Lee <mofe@me.com>
2021-07-16 21:25:22 +0800

    update preview

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-
+    
     var body: some View {
         HStack {
             landmark.image
@@ -26,6 +26,10 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
