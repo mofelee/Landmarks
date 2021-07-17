dd50ab3f6324544a6244e46e6bb5599b6dd8bdec
Mofe Lee <mofe@me.com>
2021-07-17 09:46:48 +0800

    navigate to detail

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by mofe on 2021/7/16.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
